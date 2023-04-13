3b9a8fded31dfb824a1b923ccd54f34b7c49256a
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2023-04-13 13:16:13 +0200

    Add files via upload

---
+++ 9_7Resueltas/src/Main.java
@@ -0,0 +1,33 @@
+/* 9.7
+ * Implementar la interfaz Comparable en la clase Socio para que,por defecto,
+ * se ordene segun los numeros de identificacion --id-- crecientes.
+ */
+
+ import java.util.ArrayList;
+ import java.util.Collections;
+ import java.util.List;
+ 
+ public class Main {
+     public static void main(String[] args) {
+         // Crear algunos objetos Socio
+         Socio socio1 = new Socio(1, "Juan", "01-01-1980");
+         Socio socio2 = new Socio(3, "Pedro", "01-01-1970");
+         Socio socio3 = new Socio(2, "María", "01-01-1990");
+ 
+         // Crear una lista de objetos Socio y añadir los objetos creados anteriormente
+         List<Socio> listaSocios = new ArrayList<>();
+         listaSocios.add(socio1);
+         listaSocios.add(socio2);
+         listaSocios.add(socio3);
+ 
+         
+ // Ordenar la lista de objetos Socio por id
+ Collections.sort (List<Socio>);
+ 
+ // Imprimir la lista de objetos Socio ordenada
+ for (Socio s : listaSocios) {
+     System.out.println(s);
+ }
+     }
+ }
+ 
