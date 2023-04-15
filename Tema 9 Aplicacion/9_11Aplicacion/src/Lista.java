07d35f5ff50b9b753685175e139f8f0da007ea78
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2023-04-15 12:26:42 +0200

    Add files via upload

---
+++ Tema 9 Aplicacion/9_11Aplicacion/src/Lista.java
@@ -0,0 +1,71 @@
+
+
+import java.util.Arrays;
+
+public class Lista {
+    
+    String[] tabla;
+
+    public Lista() {
+        tabla = new String[0];
+
+    }
+
+    void insertarPrincipio(String nuevo) {
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        System.arraycopy(tabla, 0, tabla, 1, tabla.length - 1);
+        tabla[0] = nuevo;
+    }
+
+    void insertarFinal(String nuevo) {
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        tabla[tabla.length - 1] = nuevo;
+    }
+
+    void insertarFinal(Lista otraLista) {
+        int tamIni = tabla.length;
+        tabla = Arrays.copyOf(tabla, tabla.length + otraLista.tabla.length);
+        System.arraycopy(otraLista.tabla, 0, tabla, tamIni, otraLista.tabla.length);
+    }
+
+    void insertar(int posicion, String nuevo) {
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        System.arraycopy(tabla, posicion, tabla, posicion + 1, tabla.length - posicion - 1);
+        tabla[posicion] = nuevo;
+
+    }
+
+    String eliminar(int indice) {
+        String eliminado = null;
+        if (indice >= 0 && indice < tabla.length) {
+            eliminado = tabla[indice];
+            for (int i = indice + 1; i < tabla.length - 1; i++) {
+                tabla[i - 1] = tabla[i];
+            }
+            tabla = Arrays.copyOf(tabla, tabla.length - 1);
+        }
+        return eliminado;
+
+    }
+
+    int buscar(Integer claveBusqueda) {
+        int indice = -1;
+        for (int i = 0; i < tabla.length && indice == -1; i++) {
+
+            if (tabla[i].equals(claveBusqueda)) {
+                indice = i;
+            }
+        }
+        return indice;
+    }
+
+    public int numeroElementos() {
+        return tabla.length;
+    }
+
+    public void mostrar() {
+        System.out.println("Lista: " + Arrays.toString(tabla));
+
+    }
+
+}
